4b181c923d20e7c7800c97faa594647ad45d0434
Rob Hamilton <Rob@Robs-MacBook-Pro.local>
2013-04-26 22:55:49 -0400

    Adding a file that uses generics in typescript.

---
+++ genericTest.ts
@@ -0,0 +1,38 @@
+// NOTE: Requires version 0.9.0.0 or greater of the Typescript compiler
+
+class GenCollection<T> {
+    collection : T[];
+
+    constructor() {
+      this.collection = new Array<T>();
+    }   
+ 
+    first() {
+      return this.collection[0];
+    }i
+
+    add(item : T) {
+      this.collection[this.collection.length] = item;
+    }
+}
+
+var a = new GenCollection<number>()
+a.add(7)
+a.add(12)
+console.log(a.first());
+console.log(JSON.stringify(a.collection));
+
+var b = new GenCollection<string>()
+b.add("Hello");
+b.add("Hi");
+console.log(b.first());
+console.log(JSON.stringify(b.collection));
+
+var c = new GenCollection<any>()
+c.add("Hello");
+c.add("Hi")
+c.add(99);
+c.add(6.2);
+c.add([1,2,3]);
+console.log(c.first());
+console.log(JSON.stringify(c.collection));
